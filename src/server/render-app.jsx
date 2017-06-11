f924383fbf627888a8534905415030ea67046dee
James Gosbell <jamesgosbell@gmail.com>
2017-06-11 19:24:04 +1000

    Updated app viewport

--- src/server/render-app.jsx
+++ src/server/render-app.jsx
@@ -31,6 +31,7 @@ const renderApp = (location: string, plainPartialState: ?Object, routerContext: 
       <head>
         ${head.title}
         ${head.meta}
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
         <link rel="shortcut icon" href="${STATIC_PATH}/images/favicon-32x32.png" type="image/x-icon" />
         <link rel="stylesheet" href="${STATIC_PATH}/css/bootstrap.min.css">
