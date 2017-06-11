1f99b8188c7bfce4fe5546e5375dab5aa10e3586
James Gosbell <jamesgosbell@gmail.com>
2017-06-11 12:19:51 +1000

    All code from steps 1-8

---
+++ src/shared/config.js
@@ -0,0 +1,26 @@
+// @flow
+
+export const WEB_PORT = process.env.PORT || 8000
+export const WDS_PORT = 7000
+export const STATIC_PATH = '/static'
+
+export const APP_NAME = 'Hello App'
+
+export const APP_CONTAINER_CLASS = 'js-app'
+export const APP_CONTAINER_SELECTOR = `.${APP_CONTAINER_CLASS}`
+
+export const HOME_PAGE_ROUTE = '/'
+export const HELLO_PAGE_ROUTE = '/hello'
+export const HELLO_ASYNC_PAGE_ROUTE = '/hello-async'
+export const NOT_FOUND_DEMO_PAGE_ROUTE = '/404'
+
+export const helloEndpointRoute = (num: ?number) => `/ajax/hello/${num || ':num'}`
+
+export const IO_CONNECT = 'connect'
+export const IO_DISCONNECT = 'disconnect'
+export const IO_CLIENT_HELLO = 'IO_CLIENT_HELLO'
+export const IO_CLIENT_JOIN_ROOM = 'IO_CLIENT_JOIN_ROOM'
+export const IO_SERVER_HELLO = 'IO_SERVER_HELLO'
+
+export const JSS_SSR_CLASS = 'jss-ssr'
+export const JSS_SSR_SELECTOR = `.${JSS_SSR_CLASS}`
